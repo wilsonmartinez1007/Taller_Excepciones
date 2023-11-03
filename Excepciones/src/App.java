9754d0ecec8f5898a3aedf66d2ddfa4b97222fad
juangabrielparedes <juan.gabriel.paredes@correounivalle.edu.co>
2023-11-02 20:53:25 -0500

    Cambios_JLabel

--- Excepciones/src/App.java
+++ Excepciones/src/App.java
@@ -1,36 +1,72 @@
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
-
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class App {
+
+public class App  extends JFrame implements ItemListener{
     Container contenedor;
     GridLayout layout;
-
+    FlowLayout layout2, layout3;
+    JComboBox<String> combo;
+    String nombre_platos[]={"","",""};
     static List<Platos> lista4 = new ArrayList<>();
 
     App(){
-        contenedor = getContentPane();
-        layout = new GridLayout(3,0)
-        contenedor.setLayout(layout);
+        this.contenedor = this.getContentPane();
+        this.layout = new GridLayout(3,0);
+        this.contenedor.setLayout(this.layout);
+        
+        this.layout2 = new FlowLayout();
+        JPanel panel1 = new JPanel(this.layout2);
+
+        JLabel etiqueta = new JLabel("                                    --Bienvenido al Restaurante a continuacion nuestro MENU--"                                                                          );
+        panel1.add(etiqueta);
+
+        Platos name;
+
+        for(int i = 0; i<lista4.size(); i++){
+            name = lista4.get(i);
+            this.nombre_platos[i] = name.nombre;
+        }
+        this.combo = new JComboBox<String>(this.nombre_platos);
+        this.combo.addItemListener(this);
+        this.contenedor.add(panel1);
 
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 600);
-        setVisible(true);
+        this.layout2 = new FlowLayout();
+        JPanel panel2 = new JPanel(this.layout);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 600);
+        this.setVisible(true);
 
     }
 
 
     public static void main(String[] args) throws Exception {
-        Platos plato1 = new Platos("Hamburgesa", "no se la verdad", Tipo_plato.Plato_fuerte, 8000, (byte)30);
+        Platos plato1 = new Platos("Hamburgesa", "no se la verdad", Tipo_plato.Entrada, 8000, (byte)30);
         Platos plato2 = new Platos("Limonada", "Jugo fresco con sabor a limonada", Tipo_plato.Bebida, 1500, (byte)10);
-        Platos plato3 = new Platos("Postre", "tampoco se la verdad", Tipo_plato.Entrada, 8000, (byte)30);
+        Platos plato3 = new Platos("Postre", "tampoco se la verdad", Tipo_plato.Plato_fuerte, 8000, (byte)30);
         lista4.add(0, plato1);
         lista4.add( 1, plato2);
         lista4.add(2, plato3);
 
         App app = new App();
+    }
+
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'itemStateChanged'");
     }
 }
